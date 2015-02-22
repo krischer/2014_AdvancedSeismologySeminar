21bfd64f56688112dbbe502a7b84695762525a2f
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-02-22 15:17:21 +0100

    Code formatting for all Python scripts in MESS folder

--- MESS/SpectralElements/Python/legendre.py
+++ MESS/SpectralElements/Python/legendre.py
@@ -1,26 +1,23 @@
-def legendre(N,x):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
 
-# returns the value of Legendre Polynomial P_N (x) at position x [-1, 1]
 
-    import numpy as np
-    
-    P = np.zeros(2*N, dtype = float)
-    
-    
-    if N==0:
-        P[0]=1
-        out=P[0]
-    elif N==1:
-        P[1]=x;
-        out=P[1];
+def legendre(N, x):
+    """
+    Returns the value of Legendre Polynomial P_N(x) at position x[-1, 1].
+    """
+    P = np.zeros(2 * N)
+
+    if N == 0:
+        P[0] = 1
+    elif N == 1:
+        P[1] = x
     else:
-        P[0]=1
-        P[1]=x
-    for i in range(2,N+1): 
-        P[i] = (float(1)/float(i))  * ( (2*i - 1)*x*P[i-1] - (i-1)*P[i-2] )
-    
-    out=P[N]
+        P[0] = 1
+        P[1] = x
+    for i in range(2, N + 1):
+        P[i] = (1.0 / float(i)) * ((2 * i - 1) * x * P[i - 1] - (i - 1) *
+                                   P[i - 2])
 
-    return(out)
-    
- 
+    return(P[N])
--- MESS/SpectralElements/Python/ricker.py
+++ MESS/SpectralElements/Python/ricker.py
@@ -1,22 +1,21 @@
-def ricker(dt,pt):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import math
+import numpy as np
 
-# RICKER generate a ricker wavelet
-# input (dt,period)
 
-    import numpy as np
-    import math as math
-
-    nt=int(2*pt/dt)
+def ricker(dt, pt):
+    """
+    RICKER generate a ricker wavelet
+    input (dt,period)
+    """
+    nt = int(2 * pt / dt)
     c = np.zeros(nt)
-    t0=pt/dt
-    a_ricker=4/pt
+    t0 = pt / dt
+    a_ricker = 4 / pt
 
-    for it in range(0,nt):
-        t=((it+1)-t0)*dt 
-        c[it] = -2*a_ricker*t * math.exp(-(a_ricker*t)**2)
-        
+    for it in range(0, nt):
+        t = ((it + 1) - t0) * dt
+        c[it] = -2 * a_ricker * t * math.exp(-(a_ricker * t) ** 2)
+
     return c
-
-
-
-
